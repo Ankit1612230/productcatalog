6dfc5c98e5e113ae75c7052a93d39866518ff4e6
Ankit1612230 <ankitp12160@gmail.com>
2026-07-14 23:13:39 +0530

    cart update

--- src/main/java/com/ecom/productcatalog/repository/CartItemRepository.java
+++ src/main/java/com/ecom/productcatalog/repository/CartItemRepository.java
@@ -3,7 +3,9 @@ package com.ecom.productcatalog.repository;
 import com.ecom.productcatalog.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 }
